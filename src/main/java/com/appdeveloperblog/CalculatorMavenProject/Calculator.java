c584ca357b9c72521f59fe21adc8470d81c937a9
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2023-10-08 12:58:07 +0530

    Excercise intergerSubstraction method

--- src/main/java/com/appdeveloperblog/CalculatorMavenProject/Calculator.java
+++ src/main/java/com/appdeveloperblog/CalculatorMavenProject/Calculator.java
@@ -2,6 +2,17 @@ package com.appdeveloperblog.CalculatorMavenProject;
 
 public class Calculator {
 	public int integerDivision(int dividend , int divisor) {
-		return dividend*divisor;
+		return dividend/divisor;
+	}
+	
+	public int integerSubstraction(int value1 , int value2) {
+		int result = 0;
+		if(value1 > value2) {
+		   result=(value1-value2);
+		}
+		if(value1 < value2) {
+		   result=(value2-value1);
+		}
+		return result;
 	}
 }
--- src/test/java/com/appdeveloperblog/CalculatorMavenProject/CalculatorTest.java
+++ src/test/java/com/appdeveloperblog/CalculatorMavenProject/CalculatorTest.java
@@ -1,15 +1,21 @@
 package com.appdeveloperblog.CalculatorMavenProject;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
 
 public class CalculatorTest {
+	Calculator calculator = new Calculator();
 
 	@Test
 	public void integerDivison() {
-		Calculator calculator = new Calculator();
 		int result = calculator.integerDivision(4,2);
 		assertEquals(2, result,"4/2 didnot produced 2");
+	}
+	
+	@Test
+	public void integerSubstraction() {
+		int result = calculator.integerSubstraction(3, 5);
+		assertEquals(2,result,"5-3 didnot produced 2");
 	}
 }
