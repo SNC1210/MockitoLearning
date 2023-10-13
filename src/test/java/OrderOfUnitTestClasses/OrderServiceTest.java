89874a2dcb25ebe7d0345e03db84dfc9beb32bbc
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2023-10-13 13:01:41 +0530

    Order Of unit Test Class

---
+++ src/test/java/OrderOfUnitTestClasses/OrderServiceTest.java
@@ -0,0 +1,20 @@
+package OrderOfUnitTestClasses;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+@Order(3)
+public class OrderServiceTest {
+	
+	@BeforeAll
+	static void setup() {
+		System.out.println("Test Order Related to Order Service");
+	}
+	
+	@Test
+	void testCreateOrder_whenProductIdIsMissing_throwsOrderServiceException() {
+		
+	}
+
+}
---
+++ src/test/java/OrderOfUnitTestClasses/ProductServiceTest.java
@@ -0,0 +1,17 @@
+package OrderOfUnitTestClasses;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+@Order(2)
+public class ProductServiceTest {
+	 @BeforeAll
+	    static void setup() {
+	        System.out.println("Test methods related to products Service ");
+	    }
+
+	    @Test
+	    void testCreateProduct_whenProductTitleIsMissing_throwsProductServiceException() {
+	    }
+}
