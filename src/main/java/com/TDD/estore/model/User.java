a256fcb47dc3f47b232624430a83cd95a089caac
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2023-10-13 18:40:40 +0530

    TDD- Test User object Contains first name

--- src/main/java/com/TDD/estore/model/User.java
+++ src/main/java/com/TDD/estore/model/User.java
@@ -2,4 +2,20 @@ package com.TDD.estore.model;
 
 public class User {
 
+	private String firstName;
+	
+	public User(String firstName) {
+		super();
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	
+
 }
--- src/test/java/com/TDD/estore/service/UserServiceTest.java
+++ src/test/java/com/TDD/estore/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.TDD.estore.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,23 @@ public class UserServiceTest {
 		
 		//Assert
 		 assertNotNull(user, "The createUser() method should not have returned null");
+	}
+	
+	@Test
+	void testCreateUser_WhenUserCreated_returnedUserObjectContainsSameFirstName() {
+		//Arrange
+		UserService userService = new UserServiceImpl(); 
+	     String firstName="AADI";
+	     String lastName="Annant";
+	     String email="annand.com";
+	     String password="1234567";
+	     String repeatPassword ="1234567";
+	     
+		
+		//Act
+	     User user= userService.createUser(firstName,lastName,email,password,repeatPassword);
+			
+		//Assert
+	     assertEquals(firstName, user.getFirstName(),"User firstname is not same");
 	}
 }
