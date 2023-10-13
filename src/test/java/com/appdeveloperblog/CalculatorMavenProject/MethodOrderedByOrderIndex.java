5d7860879b1f7aa8779f68335ed17df1e83dbe76
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2023-10-13 13:24:45 +0530

    Test instance lifecycle permethod

--- src/test/java/com/appdeveloperblog/CalculatorMavenProject/MethodOrderedByOrderIndex.java
+++ src/test/java/com/appdeveloperblog/CalculatorMavenProject/MethodOrderedByOrderIndex.java
@@ -1,34 +1,48 @@
 package com.appdeveloperblog.CalculatorMavenProject;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedByOrderIndex {
+	
+	StringBuilder completed = new StringBuilder(""); 
+	
+	@AfterEach
+	void aftereach() {
+		System.out.println("The State of completed object is:" + completed);
+	}
 	
 	@Order(1)
 	@Test
 	void testA() {
 		System.out.println("Running Test A");
+		completed.append("1");
 	}
 
 	@Order(3)
 	@Test
 	void testB() {
 		System.out.println("Running Test B");
+		completed.append("3");
 	}
 	
 	@Order(2)
 	@Test
 	void testD(){
 		System.out.println("Running Test D");
+		completed.append("2");
 	}
 	
 	@Order(4)
 	@Test
 	void testC() {
 		System.out.println("Running Test C");
+		completed.append("4");
 	}
 }
