bd192fd45eaf86d8025f2ddf950b401bfd27b733
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2023-10-13 12:02:10 +0530

    Method Order - Random order by Index

---
+++ src/test/java/com/appdeveloperblog/CalculatorMavenProject/MethodOrderedByOrderIndex.java
@@ -0,0 +1,34 @@
+package com.appdeveloperblog.CalculatorMavenProject;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderedByOrderIndex {
+	
+	@Order(1)
+	@Test
+	void testA() {
+		System.out.println("Running Test A");
+	}
+
+	@Order(3)
+	@Test
+	void testB() {
+		System.out.println("Running Test B");
+	}
+	
+	@Order(2)
+	@Test
+	void testD(){
+		System.out.println("Running Test D");
+	}
+	
+	@Order(4)
+	@Test
+	void testC() {
+		System.out.println("Running Test C");
+	}
+}
