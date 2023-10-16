5679e4f61d546f80c431f7b141aae3be70a71534
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2023-10-16 13:12:29 +0530

    Mockito Injecting User Repoitory as dependency

--- src/main/java/com/TDD/estore/service/UserServiceImpl.java
+++ src/main/java/com/TDD/estore/service/UserServiceImpl.java
@@ -3,8 +3,19 @@ package com.TDD.estore.service;
 import java.util.UUID;
 
 import com.TDD.estore.model.User;
+import com.TDDMockito.estore.data.UserRepositoryImpl;
+import com.TDDMockito.estore.data.UserRepository;
 
 public class UserServiceImpl implements UserService {
+	
+	 UserRepository  userRepository;
+
+	public UserServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+
+
 
 	@Override
 	public User createUser(String firstName,
@@ -21,7 +32,9 @@ public class UserServiceImpl implements UserService {
 			throw new IllegalArgumentException("User lastName is empty");
 		}
 		  User user = new User(firstName,lastName,email,UUID.randomUUID().toString());
-		  userRepository.save(user);
+		  
+		  boolean isUserCreated = userRepository.save(user);
+		  if(!isUserCreated) throw new UserServiceException("could not create user");
 		return user;
 		
 	}
