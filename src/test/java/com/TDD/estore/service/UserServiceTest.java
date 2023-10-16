00604436c34853b8c2d196ae4c0acc76a69e7b8d
suryanarayan chandrakar <suryanarayan1210@gmail.com>
2023-10-16 11:32:14 +0530

    TDD -Refactor Test method

--- src/test/java/com/TDD/estore/service/UserServiceTest.java
+++ src/test/java/com/TDD/estore/service/UserServiceTest.java
@@ -3,13 +3,17 @@ package com.TDD.estore.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.TDD.estore.model.User;
 
 public class UserServiceTest {
+	
+	
 
 	@Test
+	@DisplayName("User Object Created")
 	void testCreateUser_WhenUserDetailsIsProvided_returnUserObject() {
 		//Arrange
 	     UserService userService = new UserServiceImpl(); 
@@ -24,23 +28,7 @@ public class UserServiceTest {
 		
 		//Assert
 		 assertNotNull(user, "The createUser() method should not have returned null");
-	}
-	
-	@Test
-	void testCreateUser_WhenUserCreated_returnedUserObjectContainsSameFirstName() {
-		//Arrange
-		UserService userService = new UserServiceImpl(); 
-	     String firstName="AADI";
-	     String lastName="Annant";
-	     String email="annand.com";
-	     String password="1234567";
-	     String repeatPassword ="1234567";
-	     
-		
-		//Act
-	     User user= userService.createUser(firstName,lastName,email,password,repeatPassword);
-			
-		//Assert
 	     assertEquals(firstName, user.getFirstName(),"User firstname is not same");
 	}
+	
 }
